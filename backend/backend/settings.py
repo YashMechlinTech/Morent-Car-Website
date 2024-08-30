bdfacaaaac6b6426e34c5c2eab55a9bc23938548
YashMechlinTech <Yash.chauhan@mechlintech.com>
2024-08-30 20:11:59 +0530

    setup env for the supabase  storages integration .

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -177,3 +177,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # CSRF_COOKIE_SECURE = True
 
 
+
+
+DEFAULT_FILE_STORAGE='django_storage_supabase.supabase'
+SUPABASE_URL=config('SUPABASE_URL')
+SUPABASE_API_KEY=config('SUPABASE_API_KEY')
+SUPABASE_ROOT_PATH=config('SUPABASE_ROOT_PATH')
+
+
