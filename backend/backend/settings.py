2d477b7451b4b5a455149ff8fa5e2f3e2ff2d998
YashMechlinTech <Yash.chauhan@mechlintech.com>
2024-09-03 21:17:21 +0530

    removed config decouple...

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,7 +1,5 @@
 from pathlib import Path
 import os
-from decouple import config
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -10,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = 'django-insecure-#$rhiv*4%7!d)e##m^$r&j=gbp3jjk!wb@thdd%q5+vki#t7^('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
