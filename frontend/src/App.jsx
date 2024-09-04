c62b5061470326aeec4ff7763be4a03fa8a12c88
YashMechlinTech <Yash.chauhan@mechlintech.com>
2024-09-04 23:02:31 +0530

    fixed onclick rendering. added the back button added files for the login and the registration page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,13 @@
 import "./App.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HeroCards from "./components/HeroCards";
-import Destination from "./components/Destination";
-import CarList from "./components/CarList";
-import CarDetailsPage from "./components/CarDetailsPage";
+import Homepage from "./pages/Homepage";
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Header />
         <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <HeroCards />
-                <Destination />
-                <CarList />
-              </>
-            }
-          />
-          <Route path="/car/:id" element={<CarDetailsPage/>} />
+          <Route path="/" element={<Homepage />} />
         </Routes>
-        <Footer />
       </BrowserRouter>
     </>
   );
